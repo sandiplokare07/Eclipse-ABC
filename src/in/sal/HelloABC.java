dc9280a58e74eda2ed98ef2fac296b816a9dc07f
sandiplokare07 <sandiplokare07@gmail.com>
2025-08-07 17:26:15 +0530

    Add new

---
+++ src/in/sal/HelloABC.java
@@ -0,0 +1,14 @@
+package in.sal;
+
+public class HelloABC {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello ABC");
+		
+		for(int i=1;i<=5;i++)
+		{
+			System.out.print(i);
+		}
+	}
+}
